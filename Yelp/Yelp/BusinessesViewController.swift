d55e083bab86ab062adeb6273e51cd81d69c5a00
Rob Innes Hislop <rob.hislop@rd.io>
2015-04-25 17:15:55 -0700

    Prototype layout for business cell with constraints
    Also installed cocoa pods

--- Yelp/Yelp/BusinessesViewController.swift
+++ Yelp/Yelp/BusinessesViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class BusinessesViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     var businesses: [Business]!
     
     override func viewDidLoad() {
@@ -23,7 +24,9 @@ class BusinessesViewController: UIViewController {
 //                println(business.address!)
 //            }
 //        })
-        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    
         Business.searchWithTerm("Restaurants", sort: .Distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
             self.businesses = businesses
             
@@ -37,6 +40,14 @@ class BusinessesViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfRowsInSection(section: NSInteger) -> NSInteger {
+        return self.businesses.count
+    }
+    
+    func cellForRowIndexPath(tableView: UITableView, indexPath: NSIndexPath) -> UITableViewCell {
+        
     }
 
     /*
